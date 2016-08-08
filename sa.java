b55a6e8ce97440b583ef3c0c4184242bea62eaad
anusuya15 <anusuyaindhu5@gmail.com>
2016-08-08 03:40:52 -0700

    reere
    afraea

---
+++ sa.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class test6 {
+
+	public static void main(String[] args) {
+		
+				int j;
+				Scanner s=new Scanner(System.in);
+				j=s.nextInt();
+				int f=1;
+				if(j==0)
+				{
+					System.out.println("Factorial is 1");
+				}
+				else
+				{
+					do
+					{
+						f=f*i;
+						i--;
+					}
+					while(i>0);
+				}
+				System.out.println("Factorial is "+f);
+
+
+
+				
+
+			}
+
+		}
+
+	
+	 
+	
+	    
